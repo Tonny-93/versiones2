0452cf2a48252ff594f0cce72d998983a211a3ba
Tonny-93 <tonny.toscano@tecsu.edu.ec>
2025-04-29 16:17:59 -0500

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,14 @@
+n1 = int(input("ingrese primer numero "))
+n2 = int(input("ingrese segundo numero "))
+
+def promedio(num1 , num2):
+  return ((num1+num2)/2)
+
+
+def dividir(num1, num2):
+  return n1 / n2
+
+print(promedio(n1,n2))
+#print(suma(n1,n2))
+#print(multiplicacion(n1,n2))
+#print(dividir(n1,n2))
