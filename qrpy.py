45b6cdb4eb1aba2991f6e5328735391b6bcf15c1
Marlyn <marlyn.moreira@tecsu.edu.ec>
2025-05-06 18:19:28 -0500

    alan30

---
+++ qrpy.py
@@ -0,0 +1,17 @@
+valor1 = int( input("ingrese el numero"))
+valor2 = int(input("ingrese el numero"))
+valor3 = int(input("ingrese numero"))
+valor4 = int(input("ingrese numero")
+
+x=(imput"ingrese valor número 1")
+y= ("ingrese valor número 2")
+z=(imput"ingrese valor número 3")
+w=(imput"ingrese el valor 4")
+
+def operacion(x, y, z, w):
+  resultado = (w ** x) / (y ** z)
+  return str(resultado)
+
+resultado = operacion(x, y, z, w)
+print(resultado)
+
